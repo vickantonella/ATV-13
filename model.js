a0023d3879376c63862194803109787dc24cebed
Victoria Antonella <134626319+vickantonella@users.noreply.github.com>
2024-03-19 10:03:15 -0300

    Update model.js

--- model.js
+++ model.js
@@ -1,11 +1,13 @@
 // model.js
 
 const livros = [
-  { id: 1, titulo: 'Interestelar', autor: 'Christopher Nolan', ano: 2016 },
-  { id: 2, titulo: 'A Guerra dos Tronos', autor: 'George R. R. Martin', ano: 1996 },
-  { id: 3, titulo: 'Quatro Vidas de um Cachorro', autor: 'W. Bruce Cameron', ano: 1996 },
-  { id: 4, titulo: 'O Pequeno Príncipe', autor: 'Antoine de Saint-Exupéry', ano: 1943 },
-  { id: 5, titulo: 'Romeu E Julieta', autor: 'William Shakespeare', ano: 1597 }
+{ id: 1, titulo: 'O Senhor dos Anéis', autor: 'J.R.R. Tolkien', ano: 1955 },
+{ id: 2, titulo: 'Duna', autor: 'Frank Herbert', ano: 1813 },
+{ id: 3, titulo: 'Harry Potter e a Pedra Filosofal', autor: 'J.K. Rowling', ano: 1997 },
+{ id: 4, titulo: 'Fundação', autor: 'Jane Austen', ano: 1951 },
+{ id: 5, titulo: 'Orgulho e Preconceito', autor: 'J.R.R. Tolkien', ano: 1954 },
+{ id: 6, titulo: '1984', autor: 'George Orwell', ano: 1949 },
+{ id: 7, titulo: 'A Revolução dos Bichos', autor: 'George Orwell', ano: 1945 },
 ];
 
 function buscarLivroPorTitulo(titulo) {
